b5ede548ba4fdf3c809e7f4c2be531f3392661f7
KanishkSogani <kanishksogani1@gmail.com>
2025-01-10 17:03:01 +0530

    Implement user login, logout, and email verification endpoints;

---
+++ src/app/api/users/login/route.ts
@@ -0,0 +1,59 @@
+import { connect } from "@/dbConfig/dbConfig";
+import { NextRequest, NextResponse } from "next/server";
+import User from "@/models/userModel";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { email, password } = reqBody;
+
+    console.log(reqBody);
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return NextResponse.json(
+        { error: "User does not exists" },
+        { status: 500 }
+      );
+    }
+
+    console.log("User Exists");
+
+    const validPassword = await bcryptjs.compare(password, user.password);
+
+    if (!validPassword) {
+      return NextResponse.json(
+        { error: "Please check your credentials" },
+        { status: 400 }
+      );
+    }
+
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+
+    const token = jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+
+    const response = NextResponse.json({
+      message: "Logged in Successfully",
+      success: true,
+    });
+
+    response.cookies.set("token", token, {
+      httpOnly: true,
+    });
+
+    return response;
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
